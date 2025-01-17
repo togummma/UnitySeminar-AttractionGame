b7395e086abf34bcd52e7be52a103355b821ab30
togummma <129650594+togummma@users.noreply.github.com>
2025-01-18 03:05:04 +0900

    make SettingUI

--- SeminarAttractionGame/Assets/Script/UI/SettingsUI.cs
+++ SeminarAttractionGame/Assets/Script/UI/SettingsUI.cs
@@ -8,7 +8,7 @@ public class SettingsUI : MonoBehaviour
     [SerializeField] private Slider volumeSlider;
     [SerializeField] private Button closeButton;
 
-    private GameObject callerUI; // 呼び出し元のUI
+    private CanvasGroup callerUICanvasGroup; // 呼び出し元のUIのCanvasGroup
     private GameObject lastSelectedObject; // 元のUIで選択されていたオブジェクト
 
     private void Start()
@@ -43,6 +43,39 @@ public class SettingsUI : MonoBehaviour
         volumeSlider.value = GameSettings.Instance.GetVolume();
     }
 
+    public void OpenSettingsUI(CanvasGroup callerCanvasGroup)
+    {
+        // 呼び出し元UIの動作を停止
+        callerUICanvasGroup = callerCanvasGroup;
+        callerUICanvasGroup.interactable = false;
+        callerUICanvasGroup.blocksRaycasts = false;
+
+        // フォーカス情報を保存
+        lastSelectedObject = EventSystem.current.currentSelectedGameObject;
+
+        // 設定UIを表示
+        gameObject.SetActive(true);
+    }
+
+    private void CloseSettingsUI()
+    {
+        // 設定UIを非表示
+        gameObject.SetActive(false);
+
+        // 呼び出し元UIの動作を再開
+        if (callerUICanvasGroup != null)
+        {
+            callerUICanvasGroup.interactable = true;
+            callerUICanvasGroup.blocksRaycasts = true;
+        }
+
+        // 呼び出し元UIにフォーカスを戻す
+        if (lastSelectedObject != null)
+        {
+            EventSystem.current.SetSelectedGameObject(lastSelectedObject);
+        }
+    }
+
     private void OnMovementModeChanged(int selectedValue)
     {
         GameSettings.Instance.SetMode((GameSettings.MovementMode)selectedValue);
@@ -51,72 +84,5 @@ public class SettingsUI : MonoBehaviour
     private void OnVolumeChanged(float value)
     {
         GameSettings.Instance.SetVolume(value);
-    }
-
-    /// <summary>
-    /// 設定画面を開く
-    /// </summary>
-    /// <param name="callerUI">呼び出し元のUI</param>
-    public void OpenSettingsUI(GameObject callerUI)
-    {
-        // 呼び出し元UIを記録
-        this.callerUI = callerUI;
-
-        // 現在の選択オブジェクトを保存
-        lastSelectedObject = EventSystem.current.currentSelectedGameObject;
-
-        // 呼び出し元UIを無効化
-        if (callerUI != null)
-        {
-            var canvasGroup = callerUI.GetComponent<CanvasGroup>();
-            if (canvasGroup != null)
-            {
-                canvasGroup.interactable = false;
-                canvasGroup.blocksRaycasts = false;
-            }
-            else
-            {
-                callerUI.SetActive(false);
-            }
-        }
-
-        // 設定UIを表示
-        gameObject.SetActive(true);
-
-        // 最初のフォーカスを設定
-        EventSystem.current.SetSelectedGameObject(movementModeDropdown.gameObject);
-    }
-
-    /// <summary>
-    /// 設定画面を閉じる
-    /// </summary>
-    private void CloseSettingsUI()
-    {
-        // 設定UIを非表示
-        gameObject.SetActive(false);
-
-        // 呼び出し元UIを再有効化
-        if (callerUI != null)
-        {
-            var canvasGroup = callerUI.GetComponent<CanvasGroup>();
-            if (canvasGroup != null)
-            {
-                canvasGroup.interactable = true;
-                canvasGroup.blocksRaycasts = true;
-            }
-            else
-            {
-                callerUI.SetActive(true);
-            }
-        }
-
-        // 元のUIにフォーカスを戻す
-        if (lastSelectedObject != null)
-        {
-            EventSystem.current.SetSelectedGameObject(lastSelectedObject);
-        }
-
-        // 呼び出し元をクリア
-        callerUI = null;
     }
 }
