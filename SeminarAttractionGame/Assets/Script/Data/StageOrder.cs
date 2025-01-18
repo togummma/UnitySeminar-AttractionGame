95fd43e5a30f8758a813ec1fb61e0486848033e3
togummma <129650594+togummma@users.noreply.github.com>
2025-01-18 16:31:30 +0900

    Create "Stage1"

--- SeminarAttractionGame/Assets/Script/Data/StageOrder.cs
+++ SeminarAttractionGame/Assets/Script/Data/StageOrder.cs
@@ -5,6 +5,7 @@ public static class StageOrder
     // ステージ順序のリストを定義
     public static readonly string[] Stages = new string[]
     {
+        "Stage1",
         "Test1",
         "Test2",
         "Test3"
